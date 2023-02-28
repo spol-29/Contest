78286b5274efb11ea4613e199e8659007453c45e
spol-29 <polineni.sriram9@gmail.com>
2023-02-28 16:15:49 -0600

    solve usaco problems feb 2023 bronze

--- c.cpp
+++ c.cpp
@@ -1,6 +1,19 @@
 #include<bits/stdc++.h>
 using namespace std;
-int main(){
-    cin.tie(0);
-    ios_base::sync_with_stdio(false);
+int main() {
+    int n, k;
+    cin >> n >> k;
+    vector<long long> d(n);
+    for (int i = 0; i < n; i++) {
+        cin >> d[i];
+    }
+    long long cost = 0, last_end = 0;
+    for (int i = 0; i < n; i++) {
+        if (d[i] > last_end) {
+            long long duration = d[i] - last_end;
+            cost += ((duration + k - 1) / k) * k;
+            last_end = d[i] + ((duration + k - 1) / k) * k;
+        }
+    }
+    cout << cost-1 << endl;
 }
